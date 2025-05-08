c3f49882d969a87565f001f06cd3f8b3ea6687c8
Shoukat Ali <shoukatbhatt54@gmail.com>
2025-05-08 08:57:32 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/mighty/taxirider/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.sth.gb_Rides
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity: FlutterFragmentActivity() {
+}
